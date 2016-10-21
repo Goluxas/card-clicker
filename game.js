ea8a510317f34dc3a786f1cdd5e6c2fe78ca50a1
Douglas Storz <douglasstorz@gmail.com>
2016-10-21 14:54:51 -0400

    Created basic page with draw mechanic

---
+++ game.js
@@ -0,0 +1,41 @@
+var deck = {
+	init: function() {
+		this.fillDeck();
+		this.faceupCard = 'blank';
+
+		this.cacheDom();
+		this.bindEvents();
+		this.render();
+	},
+	cacheDom: function() {
+		this.$el = $("#deck-module");
+		this.$deck = this.$el.find('#deck');
+		this.$faceup = this.$el.find('#faceup');
+
+		this.decktemplate = this.$deck.find('#deck-template').html();;
+	},
+	bindEvents: function() {
+		this.$deck.on('click', this.drawCard.bind(this));
+	},
+	render: function() {
+		this.$deck.html( Mustache.render(this.decktemplate, {decksize: this.deck.length}) );
+		this.$faceup.text( this.faceupCard );
+	},
+
+	// private helpers
+	fillDeck: function() {
+		this.deck = ['plains', 'mountain', 'island']
+	},
+
+	// exposed api
+	drawCard: function() {
+		// shift() is the same as pop() but removes from the front of the list
+		var card = this.deck.shift();
+		if (card !== undefined) {
+			this.faceupCard = card;
+		}
+		this.render();
+	},
+};
+
+deck.init();
