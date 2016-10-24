a121eed19ec95bdff6d9f56f83bde6e8eab69f1a
Douglas Storz <douglasstorz@gmail.com>
2016-10-24 13:16:04 -0400

    Added time display to game.js

--- game.js
+++ game.js
@@ -8,24 +8,31 @@ var Game = (function(){
 		black: 0,
 		green: 0,
 	};
+	var time_left = 3; // number of draws remaining
 
 	// cache DOM
 	$resources = $('#resources');
 	$mana_list = $resources.find('ul');
 	mana_template = $resources.find('#mana-template').html();
+
+	$time = $('#time');
+	time_template = $time.find('#time-ticks').html();
 	
 	Mediator.on('render', render);
 	Mediator.on('drewResource', addResource);
 
 	// Draw the resource totals
 	function render() {
-		mana = []
+		var mana = []
 		Object.keys(resources).forEach(function(key) {
 			if (resources[key] > 0) {
 				mana.push({color: key, total: resources[key]});
 			}
 		});
-		$mana_list.html( Mustache.render(mana_template, {mana}) );
+		$mana_list.html( Mustache.render(mana_template, {mana: mana}) );
+
+		ticks = new Array(time_left);
+		$time.html( Mustache.render(time_template, {ticks: ticks}) );
 	}
 
 	function addResource(color) {
