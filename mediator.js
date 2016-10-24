dc2c7b2505c2f69c646a45ecd9d35968c48f8621
Douglas Storz <douglasstorz@gmail.com>
2016-10-24 13:03:13 -0400

    Added comments mediator about utilized events

--- mediator.js
+++ mediator.js
@@ -1,3 +1,14 @@
+/* This event mediator receives and forwards events
+ * among the various Card Clicker modules. Events are
+ * emitted by certain actions and then trigger a cascade
+ * response.
+ *
+ * Events currently in use:
+ * Event Name - Payload - Description
+ * drewCard - card ID - triggers onDraw effects
+ * drewResource - color - adds that color to resource totals
+ * render - n/a - tells all modules to render their data
+ */
 var Mediator = {
 	events: {},
 	on: function(eventName, fn) {
