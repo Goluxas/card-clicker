2ebd5380b752505f5130ea256c3bfd43c6270d72
Douglas Storz <douglasstorz@gmail.com>
2016-11-02 15:56:29 -0400

    Expanded the Encounter object. Added time tracking for Encounters.

--- battlefield.js
+++ battlefield.js
@@ -1,7 +1,15 @@
 var battlefield = (function() {
 
 	var army = [];
-	var encounter = '';
+	/* Encounter, when set, will be an object with the following keys:
+	 * name: 		Encounter Name
+	 * condition: 	The goal in order to beat the encounter
+	 * 				(For monster encounters, this is their attack/hp)
+	 * time_left:	The amount of time remaining to meet the condition
+	 * reward:		The reward if the encounter is successful
+	 * penalty:		The penalty if the encounter is failed
+	 */
+	var encounter = null;
 
 	// cache DOM
 	$bf = $('#battlefield');
@@ -16,6 +24,7 @@ var battlefield = (function() {
 	Mediator.on('render', render);
 	Mediator.on('addToArmy', addToArmy);
 	Mediator.on('setEncounter', setEncounter);
+	Mediator.on('drewCard', passTime);
 
 	function render() {
 		// render army
@@ -27,8 +36,11 @@ var battlefield = (function() {
 		$army_list.html( Mustache.render( army_template, {creature: army_display} ) );
 
 		// render encounter, if any
-		if (encounter !== '') {
-			$encounter.html( Mustache.render( encounter_template, {encounter: encounter} ) );
+		if (encounter !== null) {
+			ticks = new Array(encounter.time_left);
+			$encounter.html( Mustache.render( encounter_template, {enc: encounter, ticks: ticks} ) );
+		} else {
+			$encounter.html('');
 		}
 	}
 
@@ -42,6 +54,20 @@ var battlefield = (function() {
 		encounter = enc;
 
 		Mediator.emit('render');
+	}
+
+	function passTime() {
+		if (encounter !== null) {
+			encounter.time_left -= 1;
+
+			if (encounter.time_left == 0) {
+				// TODO - execute the condition and see what happens
+				// for now, just null the encounter
+				encounter = null;
+			}
+
+			Mediator.emit('render');
+		}
 	}
 
 })();
